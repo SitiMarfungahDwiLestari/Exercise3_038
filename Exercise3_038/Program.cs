62a04080fcf99d92d10711ef90ea6c7c87e5b0b4
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-11-25 16:13:32 +0700

    add method to search the specified node

--- Exercise3_038/Program.cs
+++ Exercise3_038/Program.cs
@@ -12,4 +12,24 @@ namespace Exercise3_Linked_List_A
         public string name;
         public Node next;
     }
+    class CircularList
+    {
+        Node LAST;
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public bool Search (int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);
+            }
+            if (rollNo == LAST.rollNumber)
+                return true;
+            else
+                return (false);
+        }
+    }
 }
