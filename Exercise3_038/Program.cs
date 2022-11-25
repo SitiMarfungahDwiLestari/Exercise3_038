a021d4a3a09e260c4b6fc9d1c05dcb8e6e6cb7b3
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-11-25 16:41:31 +0700

    add menu

--- Exercise3_038/Program.cs
+++ Exercise3_038/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise3_Linked_List_A
+namespace Exercise3_038
 {
     class Node
     {
@@ -38,7 +38,7 @@ namespace Exercise3_Linked_List_A
             else
                 return false;
         }
-        public bool traverse()
+        public void traverse()
         {
             if (listEmpty())
                 Console.WriteLine("\nList is empty");
@@ -62,6 +62,65 @@ namespace Exercise3_Linked_List_A
             else
                 Console.WriteLine("The first record in the list is : \n\n" + LAST.next.rollNumber + "  " + LAST.next.name);
 
+        }
+        static void Main (string[] args)
+        {
+            CircularList obj = new CircularList();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. View all the records in the list");
+                Console.WriteLine("2. Search for a record in the list");
+                Console.WriteLine("3. Display the first record int the list");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice (1-4): ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            obj.traverse();
+                        }
+                        break;
+                    case '2':
+                        {
+                            if(obj.listEmpty() == true)
+                            {
+                                Console.WriteLine("\nList is empty");
+                                break;
+                            }
+                            Node prev, curr;
+                            prev = curr = null;
+                            Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            if (obj.Search(num, ref prev, ref curr) == false)
+                                Console.WriteLine("\nRecord not found");
+                            else
+                            {
+                                Console.WriteLine("\nRecord found");
+                                Console.WriteLine("\nrollNumber: " + curr.rollNumber);
+                                Console.WriteLine("\nName: " + curr.name);
+                            }
+                        }
+                        break;
+                    case '3':
+                        {
+                            obj.firstNode();
+                        }
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option");
+                            break;
+                        }
+                }
+            }
+            /*catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }*/
         }
     }
 }
